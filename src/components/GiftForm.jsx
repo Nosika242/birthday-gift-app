edf9bec71c980e05862f147fdff0e69c873d64ec
Eze Chidera <derachukwueze@gmail.com>
2025-09-24 23:21:35 +0100

    Implemented changes on the form components

--- src/components/GiftForm.jsx
+++ src/components/GiftForm.jsx
@@ -23,7 +23,7 @@ export default function GiftForm({ gift }) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-blue-200 bg-[url(/image/background_21.jpeg)]  bg-no-repeat bg-cover bg-blend-screen">
-      <div className="bg-white p-6 rounded-xl shadow-lg w-96 relative">
+      <div className="bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500  p-6 rounded-xl shadow-lg w-96 relative">
         <h2 className="text-xl font-bold mb-4">🎁 {gift?.name}</h2>
         <form onSubmit={handleSend} className="flex flex-col gap-3">
           {/* hidden inputs for emailjs */}
@@ -34,32 +34,32 @@ export default function GiftForm({ gift }) {
             type="text"
             name="name"
             placeholder="Your Name"
-            className="border p-2 rounded"
+            className=" p-2 hover:border-emerald-50  shadow-md border-4 border-purple-500 border-y-sky-600 rounded-2xl  outline-0 shadow-emerald-300 focus:border-purple-500 focus:ring focus:ring-pink-200 focus:ring-opacity-50"
             required
           />
           <input
             type="email"
             name="email"
             placeholder="Your Email"
-            className="border p-2 rounded"
+            className="p-2 hover:border-emerald-50 shadow-md border-4 border-purple-500 border-x-indigo-500 rounded-2xl  outline-0 shadow-slate-600 focus:border-purple-500 focus:ring focus:ring-pink-200 focus:ring-opacity-50 "
             required
           />
           <textarea
             name="note"
             placeholder="Add a note (optional)"
-            className="border p-2 rounded"
+            className="border-2 p-2 rounded shadow-sm border-purple-500 border-y-sky-600 hover:border-emerald-50 outline-0 shadow-blue-400 focus:border-purple-500 focus:ring focus:ring-pink-200 focus:ring-opacity-50"
           />
           <div className="flex gap-2 mt-4">
             <button
               type="submit"
-              className="flex-1 bg-pink-500 text-white py-2 rounded hover:bg-pink-600"
+              className="flex-1 bg-pink-500 text-white py-2 rounded hover:bg-pink-600 rounded-r-2xl"
             >
               Send Request
             </button>
             <button
               type="button"
               onClick={() => setIsOpen(false)}
-              className="flex-1 bg-gray-300 py-2 rounded hover:bg-gray-400"
+              className="flex-1 bg-gray-300 py-2  hover:bg-gray-400 rounded-l-2xl"
             >
               Cancel
             </button>
